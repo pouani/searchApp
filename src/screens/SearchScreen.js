8933b5aa6de6f510f8a177f23ad35aaabfc407a1
Ulrich POUANI <pouani@medicasure.com>
2023-02-14 15:28:52 +0100

    reminder on Navigation

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,7 +4,10 @@ import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
 
-const SearchScreen = () => {
+const SearchScreen = (props) => {
+
+  console.log(props);
+  
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
 
@@ -16,20 +19,19 @@ const SearchScreen = () => {
   };
     
   return (
-    <View style={{ flex: 1 }}>
+    <>
         <SearchBar 
             term={term} 
             onTermChange={setTerm}
             onTermSubmit={() => searchApi(term)}
         />
       {errorMessage && <Text>{errorMessage}</Text>}
-      <Text>We have found {results.length} results</Text>
       <ScrollView>
         <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
         <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricer" />
         <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
       </ScrollView>
-    </View>
+    </>
   );
 };
 
