12128acd7df5af6c3d493ef05ae841513fc1db54
Ulrich POUANI <pouani@medicasure.com>
2023-02-14 15:06:47 +0100

    integrate api yelp and updated UI

---
+++ src/components/ResultsList.js
@@ -0,0 +1,34 @@
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+import React from 'react'
+import ResultsDetail from './ResultsDetail'
+
+const ResultsList = ({ title, results }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList 
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+            return <ResultsDetail result={item}/>;
+        }}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
+    },
+    container: {
+        marginBottom: 15,
+    }
+})
+
+export default ResultsList
