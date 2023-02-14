6c495bde04ad6b4973bab0fcfb1c0c9d6cc80402
Ulrich POUANI <pouani@medicasure.com>
2023-02-14 17:04:30 +0100

    One Last Fix

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,6 +4,10 @@ import React from 'react'
 import ResultsDetail from './ResultsDetail'
 
 const ResultsList = ({ title, results, navigation }) => {
+    if (!results.length) {
+        return null;
+    }
+    
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
